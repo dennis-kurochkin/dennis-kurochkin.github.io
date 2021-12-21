01b5229e61e595792f0b6e915d747371e6e5af02
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2021-12-21 21:43:06 +0300

    chore: add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>My place in the Internet {HEAD_TITLE_POSTFIX}</title>
-        <meta name={'yandex-verification'} content={'4d7bdd79ecfdf2ad'} />
+        <meta name={'yandex-verification'} content={'c10d1233246f384f'} />
         <meta name={'google-site-verification'} content={'FscL9Lr2Xiish7tUprzufCWVRN6hCAUjpFxhJrc2vqk'} />
       </Head>
       <Component {...pageProps} />
