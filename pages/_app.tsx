5ee1884d456a26be68f10eb805d8d377e76c8611
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2021-12-21 21:39:37 +0300

    chore: add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import 'modern-normalize/modern-normalize.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { HEAD_TITLE_POSTFIX } from '../domain'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My place in the Internet {HEAD_TITLE_POSTFIX}</title>
+        <meta name={'yandex-verification'} content={'4d7bdd79ecfdf2ad'} />
+        <meta name={'google-site-verification'} content={'FscL9Lr2Xiish7tUprzufCWVRN6hCAUjpFxhJrc2vqk'} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import styles from './index.module.scss'
-import Head from 'next/head'
 import Layout from '../layouts/layout'
 import BlogPostPreview from '../components/BlogPostPreview'
 import { getSortedPostsData } from '../lib/staticBlog'
 import { IBlogPostPreview } from '../domain/blogPost'
-import { HEAD_TITLE_POSTFIX } from '../domain'
 
 const MyCurrentJobLink = (
   <a
@@ -31,47 +29,42 @@ const HomePage = ({ blogPostPreviews }: InferGetStaticPropsType<typeof getStatic
   const hasBlogPostPreviews = Boolean(blogPostPreviews.length)
 
   return (
-    <>
-      <Head>
-        <title>My place in the Internet {HEAD_TITLE_POSTFIX}</title>
-      </Head>
-      <Layout
-        title={'Hey there!'}
-      >
-        <div>
-          <p className={styles.text}>
+    <Layout
+      title={'Hey there!'}
+    >
+      <div>
+        <p className={styles.text}>
             I&apos;m Dennis, a <strong>front-end engineer</strong> at {MyCurrentJobLink} located
             in <strong>Krasnodar, Russia</strong>.
-          </p>
-          <p className={styles.text}>
+        </p>
+        <p className={styles.text}>
             I love building user-friendly interfaces, writing clean self-explaining code and do my job well.<br />
             My stack is JavaScript & TypeScript, Vue, React and Node.js. But I keep learning stuff 🐱‍💻
-          </p>
-          <p className={styles.text}>
+        </p>
+        <p className={styles.text}>
             I am also professional book reader, sophisticated audiobook/podcast listener, productivity enjoyer and a fellow hiker.
-          </p>
+        </p>
+      </div>
+      {hasBlogPostPreviews && (
+        <div>
+          <h2 className={styles.blogPostsTitle}>Latest blog posts</h2>
+          <ul className={styles.blogPosts}>
+            {blogPostPreviews.map(({ id, title, publishDate }: IBlogPostPreview, index: number) => (
+              <li
+                key={index}
+                className={styles.blogPostsItem}
+              >
+                <BlogPostPreview
+                  id={id}
+                  title={title}
+                  publishDate={publishDate}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-        {hasBlogPostPreviews && (
-          <div>
-            <h2 className={styles.blogPostsTitle}>Latest blog posts</h2>
-            <ul className={styles.blogPosts}>
-              {blogPostPreviews.map(({ id, title, publishDate }: IBlogPostPreview, index: number) => (
-                <li
-                  key={index}
-                  className={styles.blogPostsItem}
-                >
-                  <BlogPostPreview
-                    id={id}
-                    title={title}
-                    publishDate={publishDate}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </Layout>
-    </>
+      )}
+    </Layout>
   )
 }
 
